d78566fb4b0952ee4250024ec98588a161acaae8
Maximilian <maxi.johannink@gmail.com>
2022-07-22 14:26:32 +0200

    Item JPG's and JavaFX

---
+++ Controller/Setup1Controller.java
@@ -0,0 +1,35 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+
+public class Setup1Controller {
+
+    private Button jaegerButton;
+    private Button schurkeButton;
+    private Button zerstoererButton;
+    private Button wildhueterButton;
+    private Button feuerkanonierButton;
+    private Button spaeherButton;
+    private Button DaemonjaegerButton;
+    private Button racheengelButton;
+    private Button kriegerButton;
+    private Button ninjaButton;
+    private Button kreuzritterButton;
+    private Button berserkerButton;
+    private Button gladiatorButton;
+    private Button kampfmoenchButton;
+    private Button todesbringerButton;
+    private Button abtruennigerButton;
+    private Button feuermagierButton;
+    private Button heiligermagierButton;
+    private Button eismagierButton;
+    private Button schattenmagierButton;
+    private Button vulkanoButton;
+    private Button gezeitenbaendigerButton;
+    private Button seherButton;
+    private Button erzmagierButton;
+    
+}
--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
     private Upgrade upgrade;
 
